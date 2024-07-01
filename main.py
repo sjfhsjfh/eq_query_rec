6e677b889134aabe751eaf1087042cb97cfd6b72
sjfhsjfh <sjfhsjfh@qq.com>
2024-07-01 14:16:02 +0800

    chore: expose main

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ from typing import List
 from model import Equation
 from query import query_eq
 
-if __name__ == "__main__":
-    from pathlib import Path
-    DIR = Path(__file__).parent
-    test_dir = DIR / "test"
-    if not test_dir.exists():
-        test_dir.mkdir()
 
-    # Translate all .typ files in test/
+def main(root):
+    if not root.exists():
+        root.mkdir()
+
     res: List[Equation] = []
-    for typ in test_dir.glob("**/*.typ"):
+    for typ in root.glob("**/*.typ"):
         try:
-            res.extend(query_eq(typ, root=DIR))
+            res.extend(query_eq(typ, root=root))
         except Exception as e:
             print(f"Error: {e}")
     import json
-    with open(test_dir / "out.json", "w") as f:
+    with open(root / "out.json", "w") as f:
         json.dump([r.reconstruct()
                   for r in res], f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    root = Path(__file__).parent
+    main(root / "test")
